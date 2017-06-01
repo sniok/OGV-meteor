8c097142c9bb69ca61ea23b61fd0ae055ddced5a
Oleksandr Dubenko <odybenko@gmail.com>
2017-06-01 20:27:54 +0200

    Working on header

--- imports/ui/components/header.js
+++ imports/ui/components/header.js
@@ -4,5 +4,8 @@ import "./header.css";
 Template.header.events({
   "click #header-profile-button": function() {
     $("#header-profile-menu").toggleClass("menu-hidden");
+  },
+  "click #header-notification-button": function() {
+    $("#header-notification-menu").toggleClass("menu-hidden");
   }
 });
