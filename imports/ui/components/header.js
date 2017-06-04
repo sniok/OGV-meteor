15ddbdae8df4b8127d29b7562fe24890560143ce
Oleksandr Dubenko <odybenko@gmail.com>
2017-06-04 16:31:39 +0200

    Landing page, footer, sign up pages

--- imports/ui/components/header.js
+++ imports/ui/components/header.js
@@ -7,5 +7,12 @@ Template.header.events({
   },
   "click #header-notification-button": function() {
     $("#header-notification-menu").toggleClass("menu-hidden");
+  },
+  "click #log-out": function(e, t) {
+    Meteor.logout(function() {
+      sAlert.info("Bye!, See you back soon");
+      Router.go("/");
+    });
+    return false;
   }
 });
